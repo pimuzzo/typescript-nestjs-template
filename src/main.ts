8e071255e0d4c3cc7e6338c1ce42940352a88f14
Simone Locci <simonelocci88@gmail.com>
2025-08-28 22:32:58 +0200

    Add logs

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { writeFileSync } from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log', 'debug'],
+  });
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, transform: true }));
 
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,21 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from "./entities/user.entity";
 
 @Injectable()
 export class UsersService {
+  private readonly logger = new Logger(UsersService.name);
   private fakeDb: User[] = [];
 
   saveUser(dto: CreateUserDto): User {
+    this.logger.debug('Creating user');
     const user: User = {
       id: this.fakeDb.length + 1,
       ...dto,
     };
     this.fakeDb.push(user);
+    this.logger.log('User created');
     return user;
   }
 
   getAllUsers(): User[] {
+    this.logger.debug('Reading all users');
     return this.fakeDb;
   }
 }
